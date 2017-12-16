2ac2d1372759df1c8eb9e0f5a2aa21a5834394a2
ckirc4 <30758803+ckirc4@users.noreply.github.com>
2017-12-16 12:25:52 +1000

    saves series of images of tree ageing

--- PruneAnimation.m
+++ PruneAnimation.m
@@ -1,13 +1,20 @@
-fileName = 'Custom_Neuron.swc'; 
+fileName = 'tree.swc'; 
 thicknessMult = 4;
 data = readSwc(fileName);
 done = false;
+age = 1;
+figure(1)
 
 while ~isempty(data) && ~done
 colours = getTypeColours(data(:,2)); 
 [cC, ~] = calculateConnections(data);
 f = drawNeuron(cC, data, thicknessMult, colours, 1);
+ylim([0, 100])
+xlim([-50, 80])
+title(sprintf('Age = %i',age));
+saveas(1,['tree_age_' num2str(age) '.png']);
 [data, done] = pruneTree(data);
+age = age + 1;
 drawnow
 pause(0.5);
 end
