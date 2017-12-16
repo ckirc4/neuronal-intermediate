4e425563e8c1ce1e0a66c65d36649d8dd6334cce
ckirc4 <30758803+ckirc4@users.noreply.github.com>
2017-12-16 12:39:02 +1000

    count number of annihilation events
    Annihilation occurs when a signal can't proceed to its neighbour(s) due to them being in the active or refractory state.

--- Simulation/simulateStep.m
+++ Simulation/simulateStep.m
@@ -1,11 +1,16 @@
-function statesN = simulateStep(statesO, dur, p, q, neighbours, rSoma)
+function [statesN, annihilationCount] = simulateStep(statesO, dur, p, q, neighbours, rSoma)
 % Updates all states and keeps track of all births
+
+if nargout < 1 || nargout > 2
+    error('Incorrect number of output variables.')
+end
 
 n = length(statesO); % Number of COMPARTMENTS
 deltaState = 1/dur; % intermediate value between 0 and 1
 epsilon = 0.0001; % accounts for computational rounding
 Random = rand(n,2); % each compartment is assigned two random values to check p and q against
 statesN = statesO; % keep track of changes in states between each step
+annihilationCount = 0;
 
 for i = [1 rSoma(end)+1:n] % consider soma as a single entity, i.e. somas comprised of a higher number of compartments won't have a higher chance of receiving signal. Point of contact to rest of dendritic tree is at first point
     if statesO(i) >= 1 - epsilon % i.e. if it is in state 1
@@ -20,6 +25,8 @@ for i = [1 rSoma(end)+1:n] % consider soma as a single entity, i.e. somas compri
             end
             if statesO(N) <= epsilon && statesN(N) <= epsilon % i.e. if neighbour is in state 0
                 statesN(N) = 1;
+            else % neighbour is in state 1
+                annihilationCount = annihilationCount + 1;
             end
         end
         
