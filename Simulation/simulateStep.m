a4f0e3d4dc2399b567c0840ba87c6cbd1b6dc637
ckirc4 <30758803+ckirc4@users.noreply.github.com>
2017-12-18 16:42:07 +1000

    fixed #2

--- Simulation/simulateStep.m
+++ Simulation/simulateStep.m
@@ -61,7 +61,9 @@ for i = [1 rSoma(end)+1:n] % consider soma as a single entity, i.e. somas compri
         end
         
         % inhibitory
-        statesN(i) = 1 - deltaState;
+        if Random(i,3) <= 0.2*p
+            statesN(i) = 1 - deltaState;
+        end
                 
     else % i.e. it must be in state 2
         statesN(i) = statesO(i) - deltaState;
